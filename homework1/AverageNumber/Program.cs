4434d5462d8cc91b04f6850ac2e01bfd2369f073
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-03-29 14:04:28 +0200

    Add files via upload

---
+++ homework1/AverageNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            bool parsingResult1 = int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Enter the second number: ");
+            bool parsingResult2 = int.TryParse(Console.ReadLine(), out int num2);
+            Console.WriteLine("Enter the third number: ");
+            bool parsingResult3 = int.TryParse(Console.ReadLine(), out int num3);
+            Console.WriteLine("Enter the forth number: ");
+            bool parsingResult4 = int.TryParse(Console.ReadLine(), out int num4);
+
+            double calcAverage = (num1 + num2 + num3 + num4) / 4;
+            Console.WriteLine($"The average number is: {calcAverage}");
+
+            Console.ReadLine();
+        }
+    }
+}
