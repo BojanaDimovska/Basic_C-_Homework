fa13f7175729f9088de803ea256f271952349b1c
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-03-29 14:05:10 +0200

    Add files via upload

---
+++ homework1/calculatorApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RealCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            bool parsingResult1 = int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Enter the second number: ");
+            bool parsingResult2 = int.TryParse(Console.ReadLine(), out int num2);
+            Console.WriteLine("Enter the operation you would want to use: +,-,* or /");
+            bool inputOperation = char.TryParse(Console.ReadLine(), out char operation);
+            if (operation == '+')
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if(operation == '-')
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if(operation == '*')
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if(operation == '/')
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid operation!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
