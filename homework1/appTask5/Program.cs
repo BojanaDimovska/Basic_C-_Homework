1171c03582df26e3c9d47712c38c16bab435b5fd
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-03-29 14:03:44 +0200

    Add files via upload

---
+++ homework1/appTask5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace appTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 1 to 3: ");
+            bool parsingResult = int.TryParse(Console.ReadLine(), out int num);
+               
+            if(num == 1)
+            {
+                Console.WriteLine("You got a new car!");
+            }
+            else if(num == 2)
+            {
+                Console.WriteLine("You got a new plane!");
+            }
+            else if(num == 3)
+            {
+                Console.WriteLine("You got a new bike!");
+            }
+
+            else
+            {
+                Console.WriteLine("You entered an invalid input!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
