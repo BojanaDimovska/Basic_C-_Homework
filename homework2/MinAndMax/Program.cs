70ea4e3a7a6fbb5bcaf3ddddcf7bce8538d20d21
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-08 17:16:41 +0200

    Add files via upload

---
+++ homework2/MinAndMax/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MinAndMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = { 30, 4, 250, 29, 56, 567, 1, 10, 43, 77 };
+            int max, min;
+
+            max = integers[0];
+            min = integers[0];
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] > max)
+                {
+                    max = integers[i];
+                }
+                if (integers[i] < min)
+                {
+                    min = integers[i];
+                }
+            }
+
+            Console.WriteLine($"Maximum is: {max}");
+            Console.WriteLine($"Minimum is: {min}");
+
+            Console.ReadLine();
+        }
+    }
+}
