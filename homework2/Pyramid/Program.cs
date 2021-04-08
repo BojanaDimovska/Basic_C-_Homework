4f165eccde61c2eafcb36f97a5c0b5345649e4ee
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-08 17:16:06 +0200

    Add files via upload

---
+++ homework2/Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, j, space, rows=4, k, t = 1;
+            
+            space = rows + 4 - 1;
+            for (i = 1; i <= rows; i++)
+            {
+                for (k = space; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", t++);
+                Console.Write("\n");
+                space--;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
