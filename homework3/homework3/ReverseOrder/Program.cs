ee6642611e0c9e3cbf5b186448c2cf658f87c0af
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-09 13:56:55 +0200

    Add files via upload

---
+++ homework3/homework3/ReverseOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, str1 = "";
+            int l;
+
+            Console.WriteLine("Input a string: ");
+            str = Console.ReadLine();
+
+            l = str.Length - 1;
+            for (int i = l; i >= 0; i--)
+            {
+                str1 = str1 + str[i];
+
+            }
+            Console.WriteLine("The string in Reverse Order is: {0}", str1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
