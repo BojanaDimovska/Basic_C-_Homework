8f95a076baf7ace62db8963fbcb1663616d0eeb0
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-09 13:55:51 +0200

    Add files via upload

---
+++ homework3/homework3/SumMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumMethod
+{
+    class Program
+    {
+        static int Sum()
+        {
+            Console.WriteLine("Input number here: ");
+            bool number = int.TryParse(Console.ReadLine(), out int num);
+
+            int sum = 0;
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+
+            }
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Sum());
+
+            Console.ReadLine();
+        }
+    }
+}
