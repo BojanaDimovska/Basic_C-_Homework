2fb1d624575bc30add0824c985601a5904b097d4
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-09 14:02:07 +0200

    Add files via upload

---
+++ homework3/homework3/BirthdayCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BirthdayCalc
+{
+    class Program
+    {
+        static int AgeCalculator(DateTime dateOfBirth)
+        {
+            DateTime today = DateTime.Now;
+            int years = today.Year - dateOfBirth.Year;
+            if (dateOfBirth > today.AddYears(-years))
+                years--;
+            return years;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your birthday date");
+            DateTime years = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("You are " + AgeCalculator(years) + " years old");
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
