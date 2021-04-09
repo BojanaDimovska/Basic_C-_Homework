5d27aedb62138c4d7cddc80ebcbee0f88c235a33
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-09 14:00:06 +0200

    Add files via upload

---
+++ homework3/homework3/Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        static int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        static int Calculator(string ops, int num1, int num2)
+        {
+
+            int result =0;
+            if (ops == "+")
+            {
+                result = Sum(num1, num2);
+            }
+            else if (ops == "-")
+            {
+                result = Sub(num1, num2);
+            }
+            else if (ops == "*")
+            {
+                result = Mul(num1, num2);
+            }
+            else if (ops == "/")
+            {
+                result = Div(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid Operator");
+            }
+
+            return result;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            bool firstNum = int.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Enter the second number");
+            bool secondNum = int.TryParse(Console.ReadLine(), out int num2);
+            Console.WriteLine("Enter operator: +, -, * or /");
+            string ops = Console.ReadLine();
+            Console.WriteLine(Calculator(ops, num1, num2));
+
+            Console.ReadLine();
+        }
+    }
+}
