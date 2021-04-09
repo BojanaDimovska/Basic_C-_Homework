dd144ebdbed9b1b0b40ca23ff32349ade3b062be
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-09 13:59:10 +0200

    Add files via upload

---
+++ homework3/homework3/FndWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FndWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a sentence: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine("The spliited sentence is: ");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
