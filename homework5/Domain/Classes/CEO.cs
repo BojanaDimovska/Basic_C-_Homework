c026edbcd01d2eac0c9adba3311e8a91e512a0d4
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-10 23:54:49 +0200

    Add files via upload

---
+++ homework5/Domain/Classes/CEO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Classes
+{
+    public class CEO : Employee
+    {
+        public Employee[] Employees { get; set; }
+        public int Shares { get; set; }
+        private double _sharesPrice { get; set; }
+
+        public CEO(Employee[] arrOfEmployees, int shares)
+        {
+            FirstName = "Ron";
+            LastName = "Ronsky";
+            Salary = 1500;
+            Employees = arrOfEmployees;
+            Shares = shares;
+        }
+
+        public double AddSharesPrice(double num)
+        {
+            return _sharesPrice = num;
+        }
+
+        public void PrintEmployees(Employee[] employees)
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.Role}");
+            }
+        }
+
+        public override double GetSalary()
+        {
+            double sharesPrice = AddSharesPrice(100);
+            return Salary + Shares * sharesPrice;
+        }
+    }
+}
