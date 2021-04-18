82990a855a53674d0b5db2701968aafb8eef39af
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-18 20:01:49 +0200

    Add files via upload

---
+++ homework6/Domain/Classes/Animal.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Domain.Classes
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+
+    }
+}
---
+++ homework6/Domain/Classes/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Classes
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
