a1a36b23cfa04afa5c24da7ceb0443c1ce47e54e
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-04-18 20:03:14 +0200

    Add files via upload

---
+++ homework6/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void SquareNumbers(List<int> listOfNumbers)
+        {
+            List<int> squareNum = listOfNumbers.Select(n => n * n).ToList();
+            foreach(int num in squareNum)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>()
+            {
+                {4 },
+                {3 },
+                {9 },
+                {5 },
+                {8 },
+                {2 },
+                {7 },
+                {4 },
+                {6 },
+                {2 },
+            };
+
+            SquareNumbers(num);
+
+            Console.ReadLine();
+        }
+    }
+}
